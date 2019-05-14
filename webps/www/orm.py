e20b6b4e2bfba37e991004596e187ff95b641c4d
Yinghuogithub <2467082718@qq.com>
2019-05-14 21:46:51 +0800

    fix orm db

--- webps/www/orm.py
+++ webps/www/orm.py
@@ -21,7 +21,7 @@ async def create_pool(loop,**kw):
         port=kw.get('port',3306),
         user=kw['user'],
         password=kw['password'],
-        bd=kw['db'],
+        db=kw['db'],
         charset=kw.get('charset','utf8'),
         autocommit=kw.get('autocommit',True),
         maxsize=kw.get('maxsize',10),
