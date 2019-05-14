c6b1688ebf23460cbdff86d26ed1e2e2304ff18f
Yinghuogithub <2467082718@qq.com>
2019-05-14 14:10:45 +0800

    add favicon.ico and markdown2.py

--- webps/www/coroweb.py
+++ webps/www/coroweb.py
@@ -146,14 +146,14 @@ def add_static(app):
     logging.info('add static %s=>%s'%('/static/',path))
 
 def add_route(app,fn):
-    method = feattr(fn,'__method__',None)
+    method = getattr(fn,'__method__',None)
     path = getattr(fn,'__route__',None)
     if path is None or method is None:
         raise ValueError('@get or @[ost not defined in %s.'%str(fn))
-    if not asynvio.iscoroutinefunction(fn) and not isnpect.isgeneratorfunction(fn):
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
         fn = asyncio.coroutine(fn)
     logging.info('add route %s %s=>%s(%s)'%(method,path,fn.__name__,','.join(inspect.signature(fn).parameters.key())))
-    app.routeer.add_route(method,apth,RequestHandler(app,fn))
+    app.routeer.add_route(method,path,RequestHandler(app,fn))
 
 def add_routes(app,module_name):
     n = module_name.rfind('.')
